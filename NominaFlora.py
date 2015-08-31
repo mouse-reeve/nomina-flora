20e8adf9bc9db12a81c1726b51d4f24451b463db
Mouse Reeve <mouse.reeve@gmail.com>
2015-08-31 13:52:26 -0700

    Basic word generation

--- NominaFlora.py
+++ NominaFlora.py
@@ -1,57 +1,27 @@
 ''' Produce latin names for flowers '''
-import random
-import re
+from WordBuilder import WordBuilder
 
 class NominaFlora(object):
-    ''' uses an existing corpus to create new phonemically consistent words '''
+    ''' Uses WordBuilder to create flower names '''
 
-    def __init__(self, initial='>', terminal='<', chunk_size=2):
-        #indicators for start and ends of words - set if necessary to avoid collision
-        self.initial = initial
-        self.terminal = terminal
-
-        self.chunk_size = chunk_size
-
-        self.links = {
-            self.initial: []
-        }
+    def __init__(self):
+        self.chunk_size = 2
+        self.genus_builder = self.get_builder('genus')
+        self.species_builder = self.get_builder('species')
 
 
-    def ingest(self, corpus_file):
-        ''' load and parse a pre-formatted and cleaned text file. Garbage in, garbage out '''
-        corpus = open(corpus_file)
-        for word in corpus.read().split(' '):
-            # clean word
-            word = word.strip()
-            word = re.sub(r'[\',\.\"]', '', word)
-
-            # iterate through n letter groups, where 1 <= n <= 3
-            n = self.chunk_size
-            start = 0
-            # >: C, Cys: t, yst: i
-            self.links[self.initial].append(word[0:n])
-            for position in range(n, len(word)):
-                start = position - n if position - n >= 0 else 0
-                base = word[start:position]
-                if not base in self.links:
-                    self.links[base] = []
-
-                self.links[base].append(word[position])
-            if not word[n:len(word)] in self.links:
-                self.links[word[n:len(word)]] = []
-            self.links[word[n:len(word)]].append(self.terminal)
+    def get_builder(self, corpus):
+        ''' creates a builder object for a wordlist '''
+        builder = WordBuilder(chunk_size=self.chunk_size)
+        builder.ingest(corpus)
+        return builder
 
 
-    def get_word(self, word=None):
-        ''' creates a new word '''
-        word = word if not word == None else self.initial
-        if not self.terminal in word:
-            word += random.choice(self.links[word[-self.chunk_size:]])
-            return self.get_word(word)
-        return word
+    def get_name(self):
+        ''' Get a new flower name '''
+        return self.genus_builder.get_word()
 
 
 if __name__ == '__main__':
-    builder = NominaFlora()
-    builder.ingest('flowers.txt')
-    print builder.get_word()
+    namer = NominaFlora()
+    print namer.get_name()

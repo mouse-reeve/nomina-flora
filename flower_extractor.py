069c4424fdf61b9d860b58fef26f7e267f5b0226
Mouse Reeve <mouse.reeve@gmail.com>
2015-08-31 18:12:59 -0700

    Adds common name files

--- flower_extractor.py
+++ flower_extractor.py
@@ -11,8 +11,15 @@ names = soup.find_all('a', href=re.compile(r'display_flower\.asp\?name\='))
 genus_file = open('genus', 'w')
 species_file = open('species', 'w')
 
+common_first = open('common_first', 'w')
+common_second = open('common_second', 'w')
+
 genus = ''
 species = ''
+
+first = ''
+second = ''
+
 for flower in names:
     name = flower.text
     parts = name.split(' ')
@@ -23,3 +30,15 @@ for flower in names:
     if species != parts[1]:
         species = parts[1]
         species_file.write('%s\n' % species)
+
+    common_name = flower.parent.next_sibling.text
+    parts = common_name.split(' ')
+    if len(parts) == 1:
+        common_second.write(common_name)
+    else:
+        if parts[0] != first:
+            first = parts[0]
+            common_first.write('%s\n' % parts[0])
+        if parts[-1] != second:
+            second = parts[-1]
+            common_second.write('%s\n' % parts[-1])
